c5fd4d2457efef1f0d21443fe2f4096d236f1142
enter-pro <github@enter.pro>
2026-06-22 02:33:32 +0000

    User edit code

--- src/pages/User/WithdrawPage.tsx
+++ src/pages/User/WithdrawPage.tsx
@@ -23,7 +23,7 @@ interface WithdrawRequest {
 
 const BANKS = ["BCA", "BNI", "BRI", "Mandiri", "BSI", "CIMB Niaga", "Danamon", "Permata", "BTN", "OVO", "GoPay", "DANA"];
 
-const MIN_WITHDRAW = 50000;
+const MIN_WITHDRAW = 150000;
 const WITHDRAW_FEE = 4500;
 
 const WithdrawPage: React.FC = () => {
